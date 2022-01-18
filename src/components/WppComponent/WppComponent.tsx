3f5331d8f9d61247eba473689d60bcb7451a5455
PedroTakahashi <pedro.takahashi@objective.com.br>
2022-01-17 23:39:59 -0300

    wpp button working

---
+++ src/components/WppComponent/WppComponent.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { OverlayTrigger, Tooltip } from 'react-bootstrap';
+import { FaWhatsapp } from 'react-icons/fa';
+
+const WppComponent = () => (
+  <OverlayTrigger
+    placement="left"
+    overlay={<Tooltip className="bg-white">Precisando de ajuda?</Tooltip>}
+  >
+    <a
+      href="https://api.whatsapp.com/send?phone=5511984002407&text=Ol%C3%A1%20M%C3%A3e%20Patr%C3%ADcia!%20Eu%20gostaria%20de%20mais%20informa%C3%A7%C3%B5es%20sobre%20como%20voc%C3%AA%20pode%20me%20ajudar."
+      target="_blank"
+      className="icon-wpp-button"
+      rel="noreferrer"
+    >
+      <i>
+        <FaWhatsapp className="wpp-icon" />
+      </i>
+    </a>
+  </OverlayTrigger>
+
+);
+
+export default WppComponent;
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import '../assets/css/App.css';
-import NavbarComponent from '../components/NavbarComponent';
+import '../assets/css/base.css';
+import NavbarComponent from '../components/NavbarComponent/NavbarComponent';
+import WppComponent from '../components/WppComponent/WppComponent';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <WppComponent />
     </div>
   );
 }
