2497c07987c061ee24296d54c8126c1217836c0f
pedrotakahashi <pedroh.takahashi@gmail.com>
2022-05-05 15:23:56 -0300

    updated master

---
+++ build/precache-manifest.1f7439eb99fbee6990d370fabbbedac0.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d2f9664d168d03a1165",
+    "url": "/maepatricia/static/css/main.1b1c16cd.chunk.css"
+  },
+  {
+    "revision": "5d2f9664d168d03a1165",
+    "url": "/maepatricia/static/js/main.5d2f9664.chunk.js"
+  },
+  {
+    "revision": "e7bf22566b9ed120b46d",
+    "url": "/maepatricia/static/css/1.bd337f89.chunk.css"
+  },
+  {
+    "revision": "e7bf22566b9ed120b46d",
+    "url": "/maepatricia/static/js/1.e7bf2256.chunk.js"
+  },
+  {
+    "revision": "39a03b3ed657afc51d5b",
+    "url": "/maepatricia/static/js/runtime~main.39a03b3e.js"
+  },
+  {
+    "revision": "60710d77ac76e4832d3a9c19fd1ec0b3",
+    "url": "/maepatricia/static/media/video1.60710d77.mp4"
+  },
+  {
+    "revision": "2ea91c10099bd7036df66ec46d18cecd",
+    "url": "/maepatricia/static/media/video2.2ea91c10.mp4"
+  },
+  {
+    "revision": "f99946f26cba88ae39ed3ee380d87d94",
+    "url": "/maepatricia/static/media/img3.f99946f2.jpeg"
+  },
+  {
+    "revision": "0030ff69da305bde599cd10a6b76cbf0",
+    "url": "/maepatricia/static/media/img4.0030ff69.jpeg"
+  },
+  {
+    "revision": "ab5536d6e321aaa1e67417e8462b2b33",
+    "url": "/maepatricia/static/media/img5.ab5536d6.jpeg"
+  },
+  {
+    "revision": "700567a8d91130853bdd315f31c5a680",
+    "url": "/maepatricia/static/media/img1.700567a8.jpeg"
+  },
+  {
+    "revision": "2135c79ab2fe4a069f9b118c4b758361",
+    "url": "/maepatricia/static/media/img2.2135c79a.jpeg"
+  },
+  {
+    "revision": "02c4d074669e6c14856e3b77d9bd4578",
+    "url": "/maepatricia/index.html"
+  }
+];
