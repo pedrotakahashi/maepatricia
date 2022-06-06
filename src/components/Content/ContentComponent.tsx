0f356ea09479ebcbdbedb3473752b4c6a15d063c
MaluBotter <mariabotter@hotmail.com>
2022-06-06 17:44:14 -0300

    chore(update-code): update code

--- src/components/Content/ContentComponent.tsx
+++ src/components/Content/ContentComponent.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import {AiOutlineStar} from 'react-icons/ai';
+import { AiOutlineStar } from 'react-icons/ai';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {
-   Container, Row, Col
-  } from 'react-bootstrap';
+  Container, Row, Col,
+} from 'react-bootstrap';
 
-import video1 from '../../assets/videos/video1.mp4';
-import video2 from '../../assets/videos/video2.mp4';
 import SocialComponent from '../SocialMediaComponent/SocialComponent';
 import CardComponent from '../CardComponent/CardComponent';
 import Photos from '../PhotosComponent/PhotosComponent';
 import { Text } from '../../Interface/constants';
+import VideoComponent from '../VideoComponent/VideoComponent';
+import img7 from '../../assets/fotos/img7.jpeg';
 
 const ContentComponent = () => (
 
@@ -19,57 +19,58 @@ const ContentComponent = () => (
     <Row>
       <Col sm={12} md={8} className="mx-auto">
         <div id="home">
-          <video className="video-class" controls src={video1}>
-            <track kind="captions" />
-            {' '}
-          </video>
+          <img
+            className="d-block w-75 mx-auto"
+            src={img7}
+            alt="First slide"
+          />
         </div>
       </Col>
     </Row>
-      <Container fluid className="about-content">
-        <Row md={12}>
-          <Col>
-            <span>
-              {Text.biography}
+    <Container fluid className="about-content">
+      <Row md={12}>
+        <Col>
+          <span>
+            {Text.biography}
           </span>
-          </Col>
-        </Row>
-      </Container>
-    <Row className="mt-2">
-      <Col md={12}>
-        <Col className="under-title" md={4}>
-          <div className="titles">
-            <AiOutlineStar className='icon-animation' />
-              Como posso te ajudar?
-            <AiOutlineStar className='icon-animation' />
-          </div>
         </Col>
-      </Col>
-    </Row>
-    <CardComponent />
-    <Row>
+      </Row>
+    </Container>
+    <Container id="services">
+      <Row className="mt-2">
+        <Col md={12}>
+          <Col className="under-title" md={4}>
+            <div className="titles">
+              <AiOutlineStar className="icon-animation" />
+              Como posso te ajudar?
+              <AiOutlineStar className="icon-animation" />
+            </div>
+          </Col>
+        </Col>
+      </Row>
+      <CardComponent />
+    </Container>
+    <Row id="videos-container">
       <Col>
         <Col className="under-title" md={12}>
           <div className="titles">
-            <AiOutlineStar className='icon-animation' />
-              Saiba mais sobre o seu futuro e acabe com seus problemas e indecisões
-            <AiOutlineStar className='icon-animation' />
+            <AiOutlineStar className="icon-animation" />
+            Saiba mais sobre o seu futuro e acabe com seus problemas e indecisões
+            <AiOutlineStar className="icon-animation" />
           </div>
         </Col>
         <Row>
           <Col className="mx-auto" sm={12} md={8}>
-          <video className="video-class" id="videos" controls src={video2}>
-            <track kind="captions" />
-          </video>
+            <VideoComponent />
           </Col>
         </Row>
         <Row>
           <Col md={12} className="mt-4 mb-4">
             <div className="titles">
-              <AiOutlineStar className='icon-animation' />
-                Acabe com as indecisões que lhe prendem no dia a dia.
-                Seja qual for o problema, eu posso ajudar.
-              <AiOutlineStar className='icon-animation' />
+              <AiOutlineStar className="icon-animation" />
+              Acabe com as indecisões que lhe prendem no dia a dia.
+              Seja qual for o problema, eu posso ajudar.
+              <AiOutlineStar className="icon-animation" />
             </div>
           </Col>
         </Row>
@@ -78,29 +79,31 @@ const ContentComponent = () => (
     <Row>
       <Container fluid className="about-content">
         <span>
-         {Text.marketing}
+          {Text.marketing}
         </span>
       </Container>
+    </Row>
+    <Container id="photos">
       <Row>
         <Col className="under-title mt-4 mb-4" md={6}>
           <div className="titles">
-            <AiOutlineStar className='icon-animation' />
-              Nosso compromisso é com você
-            <AiOutlineStar className='icon-animation' />
+            <AiOutlineStar className="icon-animation" />
+            Nosso compromisso é com você
+            <AiOutlineStar className="icon-animation" />
           </div>
         </Col>
-      </Row>   
-    </Row>
-    <Photos />
+      </Row>
+      <Photos />
+    </Container>
     <Row>
       <Col className="under-title mt-4 mb-4" md={6}>
         <div className="titles">
-          <AiOutlineStar className='icon-animation' />
-            Fale comigo agora e comece a mudar já a sua vida
-          <AiOutlineStar className='icon-animation' />
+          <AiOutlineStar className="icon-animation" />
+          Fale comigo agora e comece a mudar já a sua vida
+          <AiOutlineStar className="icon-animation" />
         </div>
       </Col>
-    </Row> 
+    </Row>
     <SocialComponent />
   </Container>
 
--- src/components/PhotosComponent/PhotosComponent.tsx
+++ src/components/PhotosComponent/PhotosComponent.tsx
@@ -9,33 +9,46 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import img4 from '../../assets/fotos/img1.jpeg';
 import img5 from '../../assets/fotos/img2.jpeg';
+import img6 from '../../assets/fotos/img6.jpeg';
+import img7 from '../../assets/fotos/img7.jpeg';
 
 const Photos = () => (
- <>
- <Container>
-   <Row id="fotos">
-     <Col>
-      <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-75 mx-auto"
-            src={img4}
-            alt="First slide"
-          />
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-75 mx-auto"
-            src={img5}
-            alt="First slide"
-          />
-        </Carousel.Item>
-      </Carousel>
-     </Col>
-   </Row>
- </Container>
- 
-</>
+  <Container>
+    <Row>
+      <Col>
+        <Carousel>
+          <Carousel.Item>
+            <img
+              className="d-block w-75 mx-auto"
+              src={img4}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-75 mx-auto"
+              src={img5}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-75 mx-auto"
+              src={img6}
+              alt="First slide"
+            />
+          </Carousel.Item>
+          <Carousel.Item>
+            <img
+              className="d-block w-75 mx-auto"
+              src={img7}
+              alt="First slide"
+            />
+          </Carousel.Item>
+        </Carousel>
+      </Col>
+    </Row>
+  </Container>
 );
 
 export default Photos;
